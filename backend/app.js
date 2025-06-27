4ac08055ce115248b9765f34f6ef7da8ab204a97
Zain Jamshaid <154011623+ZainJ5@users.noreply.github.com>
2025-06-28 01:04:46 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',       
+  origin: 'https://team-management-system-ten.vercel.app/',       
   methods: ['GET', 'POST']
 }))
 
